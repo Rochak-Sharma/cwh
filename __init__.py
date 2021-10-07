3d04a0c39e7331c4d828aa88656093ff2e2fd60c
Rochak Sharma <32054388+Rochak-Sharma@users.noreply.github.com>
2021-10-07 09:24:52 +0530

    Commit empty folder in Git with gitkeep

--- __init__.py
+++ __init__.py
@@ -10,14 +10,9 @@ from decouple import config
 from apps.config import config_dict
 from apps import create_app, db
 
-# WARNING: Don't run with debug turned on in production!
-DEBUG = config('debug', default=True, cast=bool)
-
-# The configuration
-get_config_mode = 'debug' if DEBUG else 'Production'
+get_config_mode = 'Production' 
 
 try:
-
     # Load the configuration using the default values
     app_config = config_dict[get_config_mode.capitalize()]
 
@@ -27,11 +22,5 @@ except KeyError:
 app = create_app(app_config)
 Migrate(app, db)
 
-if DEBUG:
-    app.logger.info('DEBUG       = ' + str(DEBUG))
-    app.logger.info('Environment = ' + get_config_mode)
-    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
 
-if __name__ == "__main__":
-    app.run()
  
